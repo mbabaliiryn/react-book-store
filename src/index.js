c3dc26e56c979c8b94fac58f8bdcadb6085569c1
imahnama <muannama25@gmail.com>
2020-09-21 17:11:23 +0300

    Delete unwanted files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,63 @@
 import React from 'react';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+import App from './components/App';
+import rootReducer from './reducers';
+
+const initialState = {
+  books: [
+    {
+      id: Math.round(100 * Math.random()),
+      title: 'Shreds of Tenderness',
+      category: 'Learning',
+    },
+    {
+      id: Math.round(300 * Math.random()),
+      title: 'Wheels on the Bus',
+      category: 'kids',
+    },
+    {
+      id: Math.round(300 * Math.random()),
+      title: 'Expendables',
+      category: 'Action',
+    },
+    {
+      id: Math.round(400 * Math.random()),
+      title: 'Away and Apart',
+      category: 'Sci-fi',
+    },
+    {
+      id: Math.round(500 * Math.random()),
+      title: 'The Bye Bye Man',
+      category: 'Horror',
+    },
+    {
+      id: Math.round(500 * Math.random()),
+      title: 'My Life in Crime',
+      category: 'Biography',
+    },
+  ],
+};
+
+const middleware = [thunk];
+
+const store = createStore(rootReducer, initialState,
+  compose(applyMiddleware(...middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__
+              && window.__REDUX_DEVTOOLS_EXTENSION__()));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root'),
 );
 
