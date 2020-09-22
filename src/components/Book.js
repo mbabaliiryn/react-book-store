6dd0410b37625113d45ea9068af8cebf4f3fe7b1
mbabali iryn <mbabaliiryn@gmail.com>
2020-09-22 20:34:24 +0300

    fix linter errors

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unused-prop-types */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
