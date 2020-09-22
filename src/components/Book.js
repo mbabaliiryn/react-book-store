8ea73886665fda0ad7fb5a15117c8b2725925c49
imahnama <muannama25@gmail.com>
2020-09-22 20:37:36 +0300

    Revert "Fix bug in remove button"
    This reverts commit 9184babb2c7d5f6d8ebb65d53c54467345dbe006.

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,34 +2,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = props => {
-  const { book, removeBook } = props;
-
-  const handleRemoveBook = () => removeBook(book.id);
-
-  return (
-    <tr>
-      <td>
-        {' '}
-        {book.id}
-        {' '}
-      </td>
-      <td>
-        {' '}
-        {book.title}
-        {' '}
-      </td>
-      <td>
-        {' '}
-        {book.category}
-        {' '}
-      </td>
-      <td>
-        <button type="button" onClick={() => removeBook(book.id)}>x</button>
-      </td>
-    </tr>
-  );
-};
+const Book = ({ book, handleBookRemove }) => (
+  <tr>
+    <td>
+      {' '}
+      {book.id}
+      {' '}
+    </td>
+    <td>
+      {' '}
+      {book.title}
+      {' '}
+    </td>
+    <td>
+      {' '}
+      {book.category}
+      {' '}
+    </td>
+    <td>
+      <button type="button" onClick={() => handleBookRemove(book.id)}>x</button>
+    </td>
+  </tr>
+);
 
 Book.propTypes = {
   book: PropTypes.shape({}).isRequired,
