45e6545fd65d2fa0ace2850591e08ac472aa3c6c
imahnama <muannama25@gmail.com>
2020-09-22 12:20:26 +0300

    Revert "Implement the CategoryFilter component & Keep the filter value in the Redux store"
    This reverts commit 3280ba46737c3f76a6ac64d0dae4c5f3092f3e97.

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -4,19 +4,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import CategoryFilter from '../components/CategoryFilter';
 import Book from '../components/Book';
-import { removeBook, changeFilter } from '../actions';
+import { removeBook } from '../actions';
 
-const BooksList = ({ books, removeBook, changeFilter, filter, }) => {
-  const handleFilterChange = e => {
-    const { value } = e.target;
-    changeFilter(value);
-  };
-
+const BooksList = ({ books, removeBook }) => {
   return (
     <div>
-    <CategoryFilter handleChange={handleFilterChange} />
       <table>
         <thead>
           <tr>
@@ -34,17 +27,14 @@ const BooksList = ({ books, removeBook, changeFilter, filter, }) => {
       </table>
     </div>
   );
+};
 
-  BooksList.propTypes = {
-    books: PropTypes.shape([]).isRequired,
-    filter: PropTypes.string.isRequired,
-    removeBook: PropTypes.func.isRequired,
-    changeFilter: PropTypes.func.isRequired,
-  };
+BooksList.propTypes = {
+  books: PropTypes.shape([]).isRequired,
+  removeBook: PropTypes.func.isRequired,
+};
 
-  const mapStateToProps = state => ({
-    books: state.books,
-    filter: state.filter,
-  });
-
-  export default connect(mapStateToProps, { removeBook, changeFilter })(BooksList);
+const mapStateToProps = state => ({
+  books: state.books,
+});
+export default connect(mapStateToProps, { removeBook })(BooksList);
