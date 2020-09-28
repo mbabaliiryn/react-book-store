2ff82673670d0a43cbf3f01fbbc7179c93430446
mbabali iryn <mbabaliiryn@gmail.com>
2020-09-28 12:39:21 +0300

    Updates

---
+++ precache-manifest.bca875e846d6ec62ce78789ed85bea5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8fd8c9601799ad2f7363bf358334c12",
+    "url": "/react-book-store/index.html"
+  },
+  {
+    "revision": "85f7441d92d8ab2467b6",
+    "url": "/react-book-store/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "01cb621fc98bbc76e091",
+    "url": "/react-book-store/static/css/main.226cbe39.chunk.css"
+  },
+  {
+    "revision": "85f7441d92d8ab2467b6",
+    "url": "/react-book-store/static/js/2.1f75a762.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-book-store/static/js/2.1f75a762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01cb621fc98bbc76e091",
+    "url": "/react-book-store/static/js/main.872414f3.chunk.js"
+  },
+  {
+    "revision": "6ee31f5a8c98e29f409d",
+    "url": "/react-book-store/static/js/runtime-main.9126341a.js"
+  }
+]);
